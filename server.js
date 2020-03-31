35a5667436525bf189602e562904846c99dc2971
Ivan Cruz <ivancruzhn@gmail.com>
2020-03-31 16:46:33 -0600

    heroku segundo intento

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs');
 
-const port = process.env.port || 80
+const port = process.env.PORT || 80
 
 app.use(express.static(__dirname + '/public'))
 
