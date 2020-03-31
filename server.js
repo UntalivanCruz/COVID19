643e09c64993e6f702caeb49a753821ecd06424c
Ivan Cruz <ivancruzhn@gmail.com>
2020-03-31 16:38:59 -0600

    push a heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+
+const port = process.env.port || 80
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs')
+
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear()
+})
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/confirmados', (req, res) => {
+    res.render('confirmados')
+})
+app.get('/muertos', (req, res) => {
+    res.render('muertos')
+})
+
+app.listen(port)
